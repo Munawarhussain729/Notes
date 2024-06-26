a1ef5445be0c95977c4a805fdd43e3be7c3edf5b
Munawar Hussain <munawarhussa.7@gmail.com>
2024-06-26 19:33:41 +0500

    add login api

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,6 @@ app.get('/', (req, res) => {
 
 app.post('/create-account', async (req, res) => {
   const { fullName, email, password } = req.body
-
   if (!fullName) {
     return res.status(400).json({ error: true, message: "Full name is required" })
   }
@@ -37,7 +36,6 @@ app.post('/create-account', async (req, res) => {
   if (!password) {
     return res.status(400).json({ error: true, message: 'Password is required' })
   }
-
   const isUser = await User.findOne({ email: email })
   if (isUser) {
     return res.json({
@@ -45,25 +43,54 @@ app.post('/create-account', async (req, res) => {
       message: 'User already exist'
     })
   }
-
   const user = new User({
     fullName,
     email,
     password
   })
-
   await user.save()
-
   const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: "3600m"
   })
-
   return res.json({
     error: false,
     user,
     accessToken,
     message: 'Registration Successful'
   })
+})
+
+app.post('/login', async(req, res) => {
+  const {email, password} = req.body
+  if (!email) {
+    return res.status(400).json({ error: true, message: 'Email is required' })
+  }
+  if (!password) {
+    return res.status(400).json({ error: true, message: 'Password is required' })
+  }
+  const userInfo = await User.findOne({ email: email })
+  if (!userInfo) {
+    return res.json({
+      error: true,
+      message: 'User not found'
+    })
+  }
+
+  if(userInfo.email === email  && userInfo.password === password){
+    const user = {user:userInfo}
+    const accessToken = jwt.sign({user}, process.env.ACCESS_TOKEN_SECRET,{expiresIn:'3600m'})
+    return res.json({
+      error: false,
+      email,
+      accessToken,
+      message: 'Login Successful'
+    })
+  }else{
+    return res.json({
+      error: true,
+      message: 'Invalid credentials'
+    })
+  }
 })
 app.listen(8080)
 module.exports = app
